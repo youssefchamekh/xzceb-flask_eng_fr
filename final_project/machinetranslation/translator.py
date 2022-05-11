7c26324848ad51d7b8e6c6bb168cc5ef5c4aabc2
youssefchamekh <youssef.chamekh98@gmail.com>
2022-05-11 13:21:03 +0000

    language tanslate

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,23 @@
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+translation = language_translator.translate(
+    text='Hello, how are you today?',
+    model_id='en-es').get_result()
+print(json.dumps(translation, indent=2, ensure_ascii=False))
